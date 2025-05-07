d3eb87f2e3618e5a3fc82d6177e6af54e0383826
Kuldeep Soni <142746668+rjsoni107@users.noreply.github.com>
2025-05-07 12:03:30 +0530

    Update Chat.js

--- chat-app-frontend/src/Chat.js
+++ chat-app-frontend/src/Chat.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import io from "socket.io-client";
 import axios from "axios";
-const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3005";
+const BACKEND_URL = process.env.BACKEND_URL || "https://livechatapp-m6mn.onrender.com";
 const socket = io(BACKEND_URL);
 
 function Chat() {
